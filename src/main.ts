823dd533e7e692fc7f9459ce0ede8e46b41d48b8
Wen Sun <syl1887415157@126.com>
2019-07-12 09:41:38 +0800

    feat: modified config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import {plugin} from 'vue-function-api';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.config.warnHandler = (msg, vm, trace) => {
+    console.log('WARN: ', msg);
+    console.log('Trace: ', trace);
+};
+Vue.use(plugin);
 
 new Vue({
     router,
