7c439e6935bfc29e72bd4ef4d0eaeddc6aef1d35
Wen Sun <syl1887415157@126.com>
2019-07-16 12:36:28 +0800

    feat: rewrite server using ts, and changed module to commonjs

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import Koa = require('koa');
+import Router = require('koa-router');
+import bodyParser = require('koa-bodyparser');
+import cors = require('koa2-cors');
+
+const app = new Koa();
+const router = new Router();
+
+router.post('/test', async (ctx, next) => {
+    const test = ctx.request.body.test;
+    ctx.response.body = {
+        test: 'Hello ' + test,
+    };
+    await next();
+});
+
+app.use(bodyParser());
+app.use(cors());
+app.use(router.routes());
+
+app.listen(3141);
+console.log('server started at port 3141...');
